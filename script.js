c22abe9971b56cb6fb2f471f0ee8c43ef01fc62c
Johann <johanncf@metalmat.ufrj.br>
2021-11-19 14:51:45 -0300

    Implemented shop.html with JS

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const bagDiv = document.getElementById("bag")
+var shop = []
+
+let storageList = JSON.parse(localStorage.getItem("bag"))
+
+if(storageList != null){
+   shop = storageList
+}
+
+function updateStorage(shop){
+   localStorage.setItem("bag", JSON.stringify(shop))
+}
 
 function bag(id){
-   var card = document.getElementById(id)
-   
-   bagDiv.innerHTML = card.h5.innerHTML
+   let product = {
+      img: document.getElementById(id).querySelector("img").src,
+      name: document.getElementById(id).querySelector(".card-title").innerHTML,
+      cost: document.getElementById(id).querySelector("#cost").innerHTML
+   }
 
+   console.log(product.name)
+   console.log(product.img)
+   console.log(product.cost)
+
+   shop.push(product)
+   updateStorage(shop)
 }
